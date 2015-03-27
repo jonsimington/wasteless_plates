5f7653eb9e3f02652193247e2d29af9b63ca23e0
jswv9@mst.edu <jswv9@mst.edu>
2015-03-27 02:19:36 -0500

    Change recipes app reference in settings

--- wasteless_plates/settings.py
+++ wasteless_plates/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+
+    'wasteless_plates.recipes'
 )
 
 MIDDLEWARE_CLASSES = (
