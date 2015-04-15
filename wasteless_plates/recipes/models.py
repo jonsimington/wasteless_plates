84e5c6dccce4c417a04d81106451dcb01751ab27
Connor McBride <cmm863@mst.edu>
2015-04-15 17:09:14 -0500

    Added sync.py and changed model. Sync works aside from the saving in the db.

--- wasteless_plates/recipes/models.py
+++ wasteless_plates/recipes/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=250)
     serving_size = models.IntegerField(default=0)
-    nutrition = models.OneToOneField('Nutrition')
+    bigoven_id = models.IntegerField(unique=True)
+    nutrition_info = models.OneToOneField('Nutrition')
 
 
-class Ingredients(models.Model):
+class Ingredient(models.Model):
     recipe = models.ForeignKey('Recipe')
     name = models.CharField(max_length=75)
     unit = models.CharField(max_length=25)
