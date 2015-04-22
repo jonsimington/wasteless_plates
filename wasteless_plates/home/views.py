0c124a5ebbeab28a5e386c03809e94dd5f13d4b2
Ragnyll <justanotherjakeyg@gmail.com>
2015-04-22 02:05:57 -0500

    check wasteless_plates views.py

--- wasteless_plates/home/views.py
+++ wasteless_plates/home/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 # View for /
 class HomePageView(TemplateView):
     template_name = "home/home.html"
+
+
+class MinListView(TemplateView):
+    validRecipes = Recipe.objects.filter(ingredient__item__exact())
+    
