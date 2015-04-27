9783f5f3d4fae41125d75ebba8f4d3ed1ed8a6f4
Ragnyll <justanotherjakeyg@gmail.com>
2015-04-27 16:06:06 -0500

    added the logic for the recipes selector

--- wasteless_plates/home/views.py
+++ wasteless_plates/home/views.py
@@ -3,8 +3,3 @@ from django.views.generic import TemplateView
 # View for /
 class HomePageView(TemplateView):
     template_name = "home/home.html"
-
-
-class MinListView(TemplateView):
-    validRecipes = Recipe.objects.filter(ingredient__item__exact())
-    
--- wasteless_plates/recipes/views.py
+++ wasteless_plates/recipes/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+def SearchView(request):
+    #get the string after'q=' in url -> commma seperated list of ingredients
+    query = request.GET.get('q')
+
+    #redirect to the results page
+    if query:
+        return HttpResponseRedirect("/search/results/" + query)
+    #else its invalid and send back to search bar
+    else:
+        return HttpResponseRedirect("/")
+
+class SearchResultsView(TemplateView):
+    template_name="recipes/recipes.html"
+
+    #get string from kwargs
+    query = self.kwargs['query']
+
+    #get rid of the commas
+    ingredients = [x.strip() for x in query.split(',')]
+
+    validRecipes=Recipe.objects.filter(ingredient__item__exact'ingredients')
+
+    context['recipes'] = validRecipes
+
+    return context
