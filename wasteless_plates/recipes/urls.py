88100d92ecdef9bfdb81c80f370e5b26d6cf25b6
Ragnyll <justanotherjakeyg@gmail.com>
2015-04-27 16:06:55 -0500

    added files to get some of the functions

---
+++ wasteless_plates/recipes/urls.py
@@ -0,0 +1,10 @@
+from django conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+    #The page with all the recipes
+
+    url(r'^search/$','wasteless_plates.home.views.SearchView',name='search_recipes'),
+
+    url(r'^search/results/(?P<query>[/w-]+)/$', SearchResultsView.as_view(), name="search_results")
+)
