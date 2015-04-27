e67af85a0b4d837999289fd3ff001a8c4f658585
Connor McBride <cmm863@mst.edu>
2015-04-27 17:46:41 -0500

    Added get to results. Adequately prints results. Not super-filtered, just a broad range.

--- wasteless_plates/recipes/views.py
+++ wasteless_plates/recipes/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
 from wasteless_plates.recipes.models import Recipe
-from django.views.generic.base import TemplateView
+
 # Create your views here.
 """
 def SearchView(request):
@@ -40,6 +38,30 @@ class SearchResultsView(TemplateView):
 
 
 def results(request):
+    # We're getting from the index page
+    if request.method == 'GET':
+        # This is the string that the user has entered
+        user_query = request.GET['q']
+
+        # The ingredients in the original query split by the commas
+        ingredients = user_query.split(',')
+
+        # Initialize the list and cycle through recipe objects filtering for ingredients
+        all_recipes = list()
+        for i in ingredients:
+            # We use contains here because naming conventions from BigOven are not standardized
+            all_recipes += Recipe.objects.filter(ingredient__item__name__contains=i)
+
+        # This is where we remove duplicates. This was my fault on the db end.
+        all_recipes = list(set(all_recipes))
+
+        # Return the response
+        return render_to_response('recipes.html', {
+            'recipes': all_recipes
+        })
+
+    # Should never get here but if it does, returned that they failed.
+    # (This will be apparent if they access url directly)
     return render_to_response('recipes.html', {
-        'recipes': Recipe.objects.filter(ingredient__item__name__contains='Garbage')
+        'recipes': Recipe.objects.filter(ingredient__item__name__contains='FAILED')
     })
