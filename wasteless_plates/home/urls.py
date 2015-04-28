799184d141f0fcb910b0e03df8362f47339c3fa6
Jon Simington <jon.simington@mst.edu>
2015-04-28 11:40:49 -0500

    Change to .views for python3

--- wasteless_plates/home/urls.py
+++ wasteless_plates/home/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
-from views import HomePageView
+from .views import HomePageView
+
 urlpatterns = patterns(
         '',
         url(r'^$', HomePageView.as_view(), name="home"),
