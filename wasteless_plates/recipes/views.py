3e7f44886318e1a03d9179968a477a2914ff2ba5
Connor McBride <cmm863@mst.edu>
2015-04-27 19:26:23 -0500

    idk.

--- wasteless_plates/recipes/views.py
+++ wasteless_plates/recipes/views.py
@@ -38,30 +38,31 @@ class SearchResultsView(TemplateView):
 
 
 def results(request):
-    # We're getting from the index page
-    if request.method == 'GET':
-        # This is the string that the user has entered
-        user_query = request.GET['q']
+    try:
+        # We're getting from the index page
+        if request.method == 'GET':
+            # This is the string that the user has entered
+            user_query = request.GET['q']
 
-        # The ingredients in the original query split by the commas
-        ingredients = user_query.split(',')
+            # The ingredients in the original query split by the commas
+            ingredients = user_query.split(',')
 
-        # Initialize the list and cycle through recipe objects filtering for ingredients
-        all_recipes = list()
-        for i in ingredients:
-            # We use contains here because naming conventions from BigOven are not standardized
-            all_recipes += Recipe.objects.filter(ingredient__item__name__contains=i)
+            # Initialize the list and cycle through recipe objects filtering for ingredients
+            all_recipes = list()
+            for i in ingredients:
+                # We use contains here because naming conventions from BigOven are not standardized
+                all_recipes += Recipe.objects.filter(ingredient__item__name__contains=i)
 
-        # This is where we remove duplicates. This was my fault on the db end.
-        all_recipes = list(set(all_recipes))
+            # This is where we remove duplicates. This was my fault on the db end.
+            all_recipes = list(set(all_recipes))
 
-        # Return the response
+            # Return the response
+            return render_to_response('recipes.html', {
+                'recipes': all_recipes
+            })
+    except:
+        # Should never get here but if it does, returned that they failed.
+        # (This will be apparent if they access url directly)
         return render_to_response('recipes.html', {
-            'recipes': all_recipes
+            'recipes': Recipe.objects.filter(ingredient__item__name__contains='FAILED')
         })
-
-    # Should never get here but if it does, returned that they failed.
-    # (This will be apparent if they access url directly)
-    return render_to_response('recipes.html', {
-        'recipes': Recipe.objects.filter(ingredient__item__name__contains='FAILED')
-    })
