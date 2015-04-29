c93c6af0ca66f68c1b4bdfbe89fda31899e0acbf
Connor McBride <cmm863@mst.edu>
2015-04-28 21:48:21 -0500

    Set stuff done.

--- wasteless_plates/recipes/views.py
+++ wasteless_plates/recipes/views.py
@@ -1,68 +1,37 @@
 from django.shortcuts import render_to_response
-from wasteless_plates.recipes.models import Recipe
-
-# Create your views here.
-"""
-def SearchView(request):
-    #get the string after'q=' in url -> commma seperated list of ingredients
-    query = request.GET.get('q')
-
-    #redirect to the results page
-    if query:
-        return HttpResponseRedirect("/search/results/" + query)
-    #else its invalid and send back to search bar
-    else:
-        return HttpResponseRedirect("/")
-
-
-class SearchResultsView(TemplateView):
-    #get string from kwargs
-    query = self.kwargs['query']
-
-    #get rid of the commas
-    ingredients = [x.strip() for x in query.split(',')]
-
-    shit_list = list()
-
-    for i in ingredients:
-        shit_list += Recipe.objects.filter(ingredient__item__exact='ingredients').filter()
-
-    validRecipes=Recipe.objects.filter(ingredient__item__exact='ingredients')
-
-    context = list()
-
-    context = validRecipes
-
-    return context
-"""
+from wasteless_plates.recipes.models import Recipe, Ingredient
+import random
 
 
 def results(request):
-    try:
-        # We're getting from the index page
-        if request.method == 'GET':
-            # This is the string that the user has entered
-            user_query = request.GET['q']
+    # We're getting from the index page
+    if request.method == 'GET':
+        # This is the string that the user has entered
+        user_query = request.GET['q']
 
-            # The ingredients in the original query split by the commas
-            ingredients = user_query.split(',')
+        # The ingredients in the original query split by the commas
+        ingredients = user_query.split(',')
 
-            # Initialize the list and cycle through recipe objects filtering for ingredients
-            all_recipes = list()
-            for i in ingredients:
-                # We use contains here because naming conventions from BigOven are not standardized
-                all_recipes += Recipe.objects.filter(ingredient__item__name__contains=i)
+        # Initialize the list and cycle through recipe objects filtering for ingredients
+        all_recipes = list()
 
-            # This is where we remove duplicates. This was my fault on the db end.
-            all_recipes = list(set(all_recipes))
+        # For every ingredient, we create a set in the list all_recipes including the ingredients used
+        for i in range(len(ingredients)):
+            # We use contains here because naming conventions from BigOven are not standardized
+            all_recipes.append(set(Recipe.objects.filter(ingredient__item__name__contains=ingredients[i])))
 
-            # Return the response
-            return render_to_response('recipes.html', {
-                'recipes': all_recipes
-            })
-    except:
-        # Should never get here but if it does, returned that they failed.
-        # (This will be apparent if they access url directly)
+        # This is where we actually use the intersections
+        if len(ingredients) > 1:
+            for i in range(len(ingredients) - 1):
+                all_recipes[0] = all_recipes[i].intersection(all_recipes[i+1])
+
+        # Return the response
         return render_to_response('recipes.html', {
-            'recipes': Recipe.objects.filter(ingredient__item__name__contains='FAILED')
+            'recipes': all_recipes[0]
         })
+    # Should never get here but if it does, returned that they failed.
+    # (This will be apparent if they access url directly)
+    return render_to_response('recipes.html', {
+        'recipes': Recipe.objects.filter(ingredient__item__name__contains='FAILED')
+    })
+
